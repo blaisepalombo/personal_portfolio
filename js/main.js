8d5ca6f770f2ff54fbc9adcb5c9f3fddd20d0662
Blaise Palombo <blaisepalombo@users.noreply.github.com>
2026-03-23 18:04:32 -0600

    added about page and some other small changes

--- js/main.js
+++ js/main.js
@@ -4,6 +4,30 @@ const siteNav = document.querySelector(".site-nav");
 if (menuToggle && siteNav) {
   menuToggle.addEventListener("click", () => {
     const isOpen = siteNav.classList.toggle("open");
+    menuToggle.classList.toggle("open");
     menuToggle.setAttribute("aria-expanded", isOpen);
   });
 }
+
+document.querySelectorAll('a[href]').forEach((link) => {
+  const href = link.getAttribute("href");
+
+  const isInternalLink =
+    href &&
+    !href.startsWith("http") &&
+    !href.startsWith("mailto:") &&
+    !href.startsWith("#") &&
+    !link.hasAttribute("download") &&
+    !link.getAttribute("target");
+
+  if (isInternalLink) {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.body.classList.add("page-exit");
+
+      setTimeout(() => {
+        window.location.href = href;
+      }, 220);
+    });
+  }
+});
